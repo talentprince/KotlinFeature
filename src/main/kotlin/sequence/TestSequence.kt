f4b1f9fb4397db5206eac7927d954c3196122477
Chen Chen <talent_prince@163.com>
2020-04-21 09:13:47 +0800

    Sequence sample

--- src/main/kotlin/sequence/TestSequence.kt
+++ src/main/kotlin/sequence/TestSequence.kt
@@ -3,5 +3,24 @@ package sequence
 /**
  * Iterator & Sequence
  */
-class TestSequence {
+fun main() {
+    val listOfSongs = listOf(
+        Song("冰雨", "刘德华"),
+        Song("白玫瑰", "陈奕迅"),
+        Song("惊雷", "六道"),
+        Song("浮夸", "陈奕迅"),
+        Song("无所谓", "杨坤")
+    )
+
+    listOfSongs.filter {
+        println("filter $it")
+        it.author == "陈奕迅"
+    }.map { println("map $it") }.take(1)
+
+    listOfSongs.asSequence().filter {
+        println("filter $it")
+        it.author == "陈奕迅"
+    }.map { println("map $it") }.take(1).toList()
 }
+
+data class Song(val name: String, val author: String)
